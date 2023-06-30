ee45de86164d67b74fb45faa34eb73132b9bfeb7
Muhammed Hussein Karimi <info@karimi.dev>
2023-07-01 02:45:03 +0330

    🐛 fix: some comments and config simplification
    Signed-off-by: Muhammed Hussein Karimi <info@karimi.dev>

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Start each proccess in backgroud
 bash -c 'cd /search; PORT=9090 MB__BASE_URL=http://localhost gunicorn -k "uvicorn.workers.UvicornWorker" -c "/search/gunicorn_conf.py" "app:app"' &
 /metabase/run_metabase.sh &
 nginx -g "daemon off;" &
 
+# Wait for first one to kill
 wait -n
 
+# Close script with correct exit code
 exit $?
